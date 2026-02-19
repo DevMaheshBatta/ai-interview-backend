2154dc75885174a0c01d56b36c35d1b767f670c1
DevMaheshBatta <devmaheshbatta@gmail.com>
2026-02-19 15:49:25 +0530

    .

--- app/main.py
+++ app/main.py
@@ -62,7 +62,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 app = FastAPI(title=settings.APP_NAME)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

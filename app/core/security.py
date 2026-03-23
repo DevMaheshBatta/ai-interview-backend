292816ea30d5f5d3844abb5b89f3c0ac403c5f96
DevMaheshBatta <devmaheshbatta@gmail.com>
2026-03-23 18:05:36 +0530

    forece bycrpt

--- app/core/security.py
+++ app/core/security.py
@@ -6,7 +6,7 @@ SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
